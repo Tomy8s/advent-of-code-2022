a3e0f6622d844a0de82f4d8719d42a23cdbea63c
Thomas Yates <Thomas.Yates@comparethemarket.com>
2022-12-21 08:20:39 +0000

    added day 4 task 1

---
+++ day-04/task-1.js
@@ -0,0 +1,16 @@
+const count = require('fs')
+	.readFileSync('input.txt')
+	.toString()
+	.split('\n')
+    .map(pair => pair
+        .split(',')
+        .map(range => range
+            .split('-')
+            .map(Number)))
+    .reduce((count, pair) => count + Number(
+        (pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1]) ||
+        (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1])
+    ), 0);
+
+console.log(count);
+
